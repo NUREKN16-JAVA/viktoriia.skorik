5ca6028fe922a07e9430c3ff8d5392bb677791ee
ViiSkor <viktoriia.skorik@nure.ua>
2018-12-24 14:03:31 +0200

    AddServlet implementation

--- usermanagement/src/main/java/ua/nure/kn/skorik/usermanagement/web/AddServlet.java
+++ usermanagement/src/main/java/ua/nure/kn/skorik/usermanagement/web/AddServlet.java
@@ -1,7 +1,22 @@
 package ua.nure.kn.skorik.usermanagement.web;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
-public class AddServlet extends HttpServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import ua.nure.kn.skorik.usermanagement.User;
+import ua.nure.kn.skorik.usermanagement.db.DaoFactory;
+import ua.nure.kn.skorik.usermanagement.db.DatabaseException;
+
+public class AddServlet extends EditServlet {
+	
+	protected void showPage(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/add.jsp").forward(req, resp);
+	}
+
+	protected void processUser(User user) throws DatabaseException {
+		DaoFactory.getInstance().getUserDao().create(user);
+	}
 }
